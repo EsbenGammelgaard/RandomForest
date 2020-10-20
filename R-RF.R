7720ea6e2278512d519ec836160d43a047a449b8
EsbenGammelgaard <58259763+EsbenGammelgaard@users.noreply.github.com>
2020-10-20 11:05:42 +0200

    Add files via upload

--- R-RF.R
+++ R-RF.R
@@ -6,47 +6,36 @@ mnist_raw <-
   read.csv(file.path("C:\\Users\\esbe1\\Desktop\\Portfolio", "mnist_train.csv"))
 
 # Required inputs
-split <- 50  # Split size where 90 means that it is 90/10 
-size_matrix <- c(1000, 2000, 3000, 4000)
-ntree_matrix <- c(1, 3, 5, 10, 20)
+split <- 50  # Split size where 90 means that it is 90/10
+size <- 2000
+ntree <- 5
 
-# Creating results Matrix
-results <- matrix(1:20, nrow = 5)
-results_time <- matrix(1:20, nrow = 5)
+# Splitting between train and test data
+start <- Sys.time()
+mnist_train <- mnist_raw[1:(size * split * 0.01), -1]
+mnist_test <- mnist_raw[(size * split * 0.01 + 1):size, -1]
 
-colnames(results) <- size_matrix
-rownames(results) <- ntree_matrix
-colnames(results_time) <- size_matrix
-rownames(results_time) <- ntree_matrix
+# Splitting between train and test labels
+mnist_train_labels <-
+  factor(mnist_raw[1:(size * split * 0.01), 1])
+mnist_test_labels <-
+  factor(mnist_raw[(size * split * 0.01 + 1):size, 1])
 
-for (ntree in ntree_matrix){
-  for (size in size_matrix){
-    start <- Sys.time()
-    
-    # Splitting between train and test data
-    mnist_train <- mnist_raw[1:(size * split * 0.01), -1]
-    mnist_test <- mnist_raw[(size * split * 0.01 + 1):size, -1]
-    
-    # Splitting between train and test labels
-    mnist_train_labels <- factor(mnist_raw[1:(size * split * 0.01), 1])
-    mnist_test_labels <-
-      factor(mnist_raw[(size * split * 0.01 + 1):size, 1])
-    
-    # Training the algorithm
-    rf <- randomForest(mnist_train_labels~., data = mnist_train, ntree = ntree)
-    
-    # Predicting the test data
-    mnist_pred <- predict(rf,mnist_test,type = 'class')
-    
-    # Confusion Matrix and calculation of overall accuracy
-    cf_half_ind  <- confusionMatrix(mnist_test_labels , mnist_pred)
-    cf_half_ind$table
-    acc_half_ind <-
-      sum(diag(cf_half_ind$table)) / sum(cf_half_ind$table)
-    
-    #logging results
-    stop <- Sys.time()
-    results[toString(ntree),toString(size)] <- acc_half_ind
-    results_time[toString(ntree),toString(size)] <- stop - start
-    
-}}
+# Training the algorithm
+rf <-
+  randomForest(mnist_train_labels ~ ., data = mnist_train, ntree = ntree)
+
+# Predicting the test data
+mnist_pred <- predict(rf, mnist_test, type = 'class')
+
+# Confusion Matrix and calculation of overall accuracy
+cf_m  <- confusionMatrix(mnist_test_labels , mnist_pred)
+acc <-
+  sum(diag(cf_m$table)) / sum(cf_m$table)
+
+#logging results
+stop <- Sys.time()
+time <- stop - start
+write.table(cf_m$table,
+            "C:/Users/esbe1/Desktop/Portfolio/R-RF-CF.txt",
+            sep = ",")
